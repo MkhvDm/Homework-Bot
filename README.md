bd127b824e51097abcf060b9c18e19c7b1a8781f
Dmitriy Mikheev <mkhvdm@gmail.com>
2022-06-28 10:53:35 +0300

    start_save

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homework_bot
-python telegram bot
+Telegram bot that notifies you about your homework status.
+
+Quick start:
+1. > $ python -m venv venv
+2. > $ source venv/Scripts/activate
+3. > $ pip install -r requirements.txt
+   
+Then, add your tokens: Telegram API token, Practicum token.
+
+Enjoy!
--- homework.py
+++ homework.py
@@ -1,15 +1,34 @@
-...
+import os
+import sys
+import time
+import requests
+from pprint import pprint
+
+import logging
+from logging import StreamHandler
+
+from telegram import ReplyKeyboardMarkup, Bot
+from telegram.ext import Updater, CommandHandler
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
+# LOGGING
+FORMATTER = logging.Formatter(
+    '%(asctime)s - [%(levelname)s] %(name)s - %(message)s'
+)
+# LOG_FILE = "bot_log.log"
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')  # bot
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')  # me
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+payload = {'from_date': 0}  #???
 
 
 HOMEWORK_STATUSES = {
@@ -19,64 +38,76 @@ HOMEWORK_STATUSES = {
 }
 
 
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(get_console_handler())
+    logger.propagate = False  # todo GOOGLE???
+    return logger
+
+
 def send_message(bot, message):
-    ...
+    pass
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
-    ...
+    pass
 
 
 def check_response(response):
-
-    ...
+    pass
 
 
 def parse_status(homework):
     homework_name = ...
     homework_status = ...
-
-    ...
-
+    #
     verdict = ...
-
-    ...
-
+    #
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    pass
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    bot_logger = get_logger(__name__)
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+    logging.info(f'Current time: {current_timestamp}')
 
-    ...
+    # ...
 
     while True:
         try:
-            response = ...
+            response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+            pprint(response.text)
+            pprint(response.json())
+            # ...
 
-            ...
-
-            current_timestamp = ...
+            current_timestamp = int(time.time())  # ???
             time.sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            # ...
             time.sleep(RETRY_TIME)
         else:
-            ...
+            pass
+            # ...
 
 
 if __name__ == '__main__':
